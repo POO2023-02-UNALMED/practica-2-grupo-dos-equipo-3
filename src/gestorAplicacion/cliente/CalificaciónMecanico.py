5402cf2407db9bf9cdd9487b8f144f73822d300d
Angelika Moya <angelmb023@gmail.com>
2023-11-14 18:50:54 -0500

    update CalificacionMecanico y TipoDaño
    Se crean las clases TipoDaño y CalificacionMecanico

--- src/gestorAplicacion/cliente/CalificaciónMecanico.py
+++ src/gestorAplicacion/cliente/CalificaciónMecanico.py
@@ -1,2 +1,28 @@
-class CalificaciónMecanico:
-    pass
+from abc import ABC,abstractclassmethod
+
+
+class CalificacionMecanico(ABC):
+    
+    @abstractclassmethod
+    def mecanicoSeleccionado(self, seleccion):
+        pass
+    
+    @abstractclassmethod
+    def mecanicosActivosCliente(self):
+        pass
+    
+    @abstractclassmethod
+    def calificarMecanico(self, mecanico, calificacion):
+        pass
+    
+    @abstractclassmethod
+    def dejarComisionMecanico(self, mecanico, comision):
+        pass
+    
+    @abstractclassmethod
+    def premiarPorEncuesta(self, mecanico, administrador):
+        pass
+    
+    @abstractclassmethod
+    def calificarTaller(self, admin, calificacion):
+        pass
